4045bd6b1b9d2dd5036e5b2d6d66e90287135ab2
lananh1983 <88231029+lananh1983@users.noreply.github.com>
2024-04-10 08:02:55 +0000

    add dayjs

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const dayjs = require('dayjs')
+
+dayjs().second(30).valueOf() // => new Date().setSeconds(30)
+dayjs().second() // => new Date().getSeconds()
+
+console.log(dayjs().second(30).valueOf())
